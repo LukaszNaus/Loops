6df7724a97171820a601b8f7ffec835c6c9756c1
LukaszNaus <nauslukasz@gmail.com>
2022-09-20 20:03:04 +0200

    Added for each loop

---
+++ src/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        //ForEachLoop
+        String[] cars = new String[]{"Audi", "BMW", "Mercedes", "Toyota"};
+        for (String car : cars) {
+            System.out.println(car);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //For loop
         String[] cars=new String[]{"Audi", "BMW", "Mercedes", "Toyota"};
-        for (int i=0; i<4; i++){
+        for (int i=0; i< cars.length; i++){
             System.out.println(cars[i]);
         }
     }
